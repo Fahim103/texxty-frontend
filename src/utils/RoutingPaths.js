2b023faeeec4e53412471029c1b85dc20ff542b4
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2020-04-11 21:30:36 +0600

    Added utility file to manage frontend routing paths

---
+++ src/utils/RoutingPaths.js
@@ -0,0 +1,15 @@
+export function GetApiRootUrl(){
+    return 'https://localhost:44367';
+}
+
+export function UserLoginRoute(){
+    return '/Users/Login';
+}
+
+export function UserRegisterRoute(){
+    return '/Users/Register';
+}
+
+export function AdminLoginRoute(){
+    return '/Admin/Login';
+}
